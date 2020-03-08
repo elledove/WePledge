02bf6cb94b7257c4a8bb7c3883badd5537fff305
Lauren Dove <elledoveinc@gmail.com>
2020-03-08 09:43:32 -0700

    Have orgz model validate presence of name, create private section for custom params for donationscontroller. Work on create action on donationscontroller

--- app/controllers/donations_controller.rb
+++ app/controllers/donations_controller.rb
@@ -10,8 +10,20 @@ class DonationsController < ApplicationController
     end
 
     def create
+        @donation = Donation.new(donations_params)
+        if @donation.save
+           redirect_to new_donations_organizations_path(@donation)
+        else 
+            render :new
+        end
     end
 
     def show
+    end
+
+    private
+
+    def  donations_params
+        params.require(:donations).permit(:amount, :description)
     end
 end
--- app/models/organization.rb
+++ app/models/organization.rb
@@ -1,4 +1,5 @@
 class Organization < ApplicationRecord
 has_many :donations #(organization.donations)
 has_many :users, through: :donations #gives methods related to users (organiznation.users/)
+validates_presence_of :name
 end
